0d8082d57788efe286eb5df28f1dde1deb7cfef4
Bryan Calisto <bryancalisto@yahoo.es>
2021-05-12 01:00:43 -0500

    Chore: Processor control opcodes are implemented

--- src/i8086.c
+++ src/i8086.c
@@ -4,6 +4,14 @@ Intel 8086 processor emulator
 
 #include "i8086.h"
 
+/*To facilitate the parsing of mod_reg_r/m byte*/
+typedef struct mod_reg_rm
+{
+  uint8_t mod;
+  uint8_t reg;
+  uint8_t r_m;
+} mod_reg_rm;
+
 /* GENERIC MEMORY OPS */
 
 // Read byte from memory
@@ -72,6 +80,14 @@ static inline bool parity(uint16_t data)
   return (one_bits & 1) == 0;
 }
 
+/* Util to set flags that are commonly set together: Zero, signed, parity*/
+void set_ZSP(i8086 *const p, uint16_t val)
+{
+  p->zf = val == 0;
+  p->sf = (val >> 15);
+  p->pf = parity(val);
+}
+
 /* 
 'bits' parameter is used to define the size of the summands in bits (this carry detector 
 could be used for 4, 8 and 16 bit numbers).
@@ -81,4 +97,187 @@ static inline bool carry(uint8_t bits, uint16_t a, uint16_t b, bool carr)
   int16_t result = a + b + carr;
   int16_t carry = result ^ a ^ b;
   return carry & (1 << bits);
+}
+
+/* Opcode helpers*/
+/*
+Checks if last byte of opcode ('w', according to the intel manual) is 1 or 0. 
+If 1, then it's a word operation, else it's a byte operation
+*/
+static inline bool is_word_op(uint8_t byte)
+{
+  return (byte & 0b1);
+}
+
+static inline bool is_destiny_to(uint8_t byte)
+{
+  return (byte & 0b10);
+}
+
+static inline uint8_t get_mod(uint8_t byte)
+{
+  // Mod is defined by the two highest bits
+  return (byte >> 6) & 0b11;
+}
+
+static inline uint8_t get_r_m(uint8_t byte)
+{
+  return (byte >> 3) & 0b111;
+}
+
+/*Helper to decode the second byte of an instruction*/
+void decodeSecondOp(mod_reg_rm *mrrm, uint8_t op)
+{
+  mrrm->mod = (op >> 6) & 0b11;
+  mrrm->r_m = (op >> 3) & 0b111;
+  mrrm->reg = op & 0b111;
+}
+
+/*ARITHMETIC*/
+static inline void i8086_add(i8086 *const p, uint8_t *const reg, uint8_t val, bool cy)
+{
+  uint8_t result = *reg + val + cy;
+  p->cf = carry(8, *reg, val, cy);
+  SET_ZSP(p, result);
+  *reg = result;
+}
+
+void i8086_exec(i8086 *const p, mod_reg_rm *m_r_rm, uint8_t opcode)
+{
+  uint8_t mod;
+  uint8_t next_b_1;
+  uint8_t next_b_2;
+  uint16_t next_w_1;
+  uint16_t next_w_2;
+
+  switch (opcode)
+  {
+  /*DATA TRANSFER*/
+  /*Register/Memory to/from Register*/
+  case 0x88:
+    break;
+  case 0x89:
+    break;
+  case 0x8A:
+    break;
+  case 0x8B:
+    break;
+  case 0xA0:
+    break;
+  case 0xA1:
+    break;
+  case 0xA2:
+    break;
+  case 0xA3:
+    break;
+  case 0xB0:
+    break;
+  case 0xB1:
+    break;
+  case 0xB2:
+    break;
+  case 0xB3:
+    break;
+  case 0xB4:
+    break;
+  case 0xB5:
+    break;
+  case 0xB6:
+    break;
+  case 0xB7:
+    break;
+  case 0xB8:
+    break;
+  case 0xB9:
+    break;
+  case 0xC0:
+    break;
+  case 0xC1:
+    break;
+  case 0xC2:
+    break;
+  case 0xC3:
+    break;
+  case 0xC4:
+    break;
+  case 0xC5:
+    break;
+    /*Immediate to register/memory*/
+  case 0xC6: // byte
+    uint8_t op = i8086_next_byte(p);
+    decodeSecondOp(m_r_rm, op);
+    uint8_t val = i8086_next_byte(p);
+    if (m_r_rm->mod == 0b11) // immediate -> register
+    {
+    }
+    break;
+  case 0xC7: // word
+    break;
+  case 0x8C:
+    break;
+  case 0x8E:
+    break;
+
+  /*Processor control*/
+
+  // Clear carry
+  case 0xF8:
+    p->cf = 0;
+    break;
+  // Complement carry
+  case 0xF5:
+    p->cf = !p->cf;
+    break;
+  // Set carry
+  case 0xF9:
+    p->cf = 1;
+    break;
+  // Clear direction
+  case 0xFC:
+    p->df = 0;
+    break;
+  // Set direction
+  case 0xFD:
+    p->df = 1;
+    break;
+  // Clear interrupt
+  case 0xFA:
+    p->iif = 0;
+    break;
+  // Set interrupt
+  case 0xFB:
+    p->iif = 1;
+    break;
+  // Halt
+  case 0xF4:
+    p->halted = 1;
+    break;
+  // Wait
+  case 0x9B:
+    p->interrupt_waiting = 1;
+    break;
+  // Escape (to external device)
+  // NOT IMPLEMENTED YET
+  case 0xD8:
+    break;
+  case 0xD9:
+    break;
+  case 0xDA:
+    break;
+  case 0xDB:
+    break;
+  case 0xDC:
+    break;
+  case 0xDD:
+    break;
+  case 0xDF:
+    break;
+  // Bus lock prefix
+  // NOT IMPLEMENTED YET
+  case 0xF0:
+    break;
+
+  default:
+    break;
+  }
 }
